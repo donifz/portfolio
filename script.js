2a11e12cbdc9dfc733cbe28b9bf9d9f1d036d2ed
donifz <doniattokurovich@gmail.com>
2026-04-07 11:48:09 +0000

    pages

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-// Плавная прокрутка по якорям
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        if (target) {
-            target.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            });
-        }
-    });
+// Анимация загрузки страницы справа
+const pageShell = document.querySelector('.page-shell');
+window.addEventListener('load', () => {
+    if (pageShell) {
+        pageShell.classList.add('slide-in-right');
+    }
 });
 
 // Обработка формы контактов
